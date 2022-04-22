2e28b4c73d111af70c09063fc1015c1698e745ba
elcia-3 <elciawork@gmail.com>
2022-04-22 09:15:05 +0900

    fix: todaysPomodoro

--- electron-src/index.ts
+++ electron-src/index.ts
@@ -1,191 +1,227 @@
 // Native
-import { join } from 'path'
-import { format } from 'url'
-const electron = require('electron')
+import { join } from "path";
+import { format } from "url";
+const electron = require("electron");
 
 // Packages
-import { BrowserWindow, app, ipcMain, Notification } from 'electron'
-import isDev from 'electron-is-dev'
-import prepareNext from 'electron-next'
+import { BrowserWindow, app, ipcMain, Notification } from "electron";
+import isDev from "electron-is-dev";
+import prepareNext from "electron-next";
 
 // Prepare the renderer once the app is ready
-app.on('ready', async () => {
-  await prepareNext('./renderer')
+app.on("ready", async () => {
+  await prepareNext("./renderer");
 
-  var fs = require('fs');
+  var fs = require("fs");
   var info_path = path.join(app.getPath("userData"), "bounds-info.json");
-  var bounds_info : any;
+  var bounds_info: any;
   try {
-      bounds_info = JSON.parse(fs.readFileSync(info_path, 'utf8'));
-  }
-  catch(e) {
-      bounds_info = {width: 740, height: 907}; 
+    bounds_info = JSON.parse(fs.readFileSync(info_path, "utf8"));
+  } catch (e) {
+    bounds_info = { width: 740, height: 907 };
   }
 
-
-  const mainWindow = new BrowserWindow(setWindow())
-  mainWindow.on('resize', WindowResize);
-  mainWindow.on('close', function() {
+  const mainWindow = new BrowserWindow(setWindow());
+  mainWindow.on("resize", WindowResize);
+  mainWindow.on("close", function () {
     fs.writeFileSync(info_path, JSON.stringify(mainWindow.getBounds()));
-    console.log(bounds_info)
+    console.log(bounds_info);
   });
 
-
   let currentWindowSize = mainWindow.getSize();
-  let NextWindowSize =  mainWindow.getSize();
+  let NextWindowSize = mainWindow.getSize();
 
-  const Screen = electron.screen
+  const Screen = electron.screen;
 
   function WindowResize() {
-    NextWindowSize =  mainWindow.getSize();
+    NextWindowSize = mainWindow.getSize();
 
-    const currentDisplay = Screen.getDisplayNearestPoint(Screen.getCursorScreenPoint());
+    const currentDisplay = Screen.getDisplayNearestPoint(
+      Screen.getCursorScreenPoint()
+    );
     const size = currentDisplay.size;
     console.log(size.width);
     console.log(size.height);
-    if(NextWindowSize[1] < size.height - 100 ){
-      if((currentWindowSize[0] < NextWindowSize[0]) || (currentWindowSize[0] > NextWindowSize[0])){
-          mainWindow.setSize(NextWindowSize[0],NextWindowSize[0] + 210 ) ;
-      }else{
-          mainWindow.setSize(NextWindowSize[1] - 210 ,NextWindowSize[1]) ;
+    if (NextWindowSize[1] < size.height - 100) {
+      if (
+        currentWindowSize[0] < NextWindowSize[0] ||
+        currentWindowSize[0] > NextWindowSize[0]
+      ) {
+        mainWindow.setSize(NextWindowSize[0], NextWindowSize[0] + 210);
+      } else {
+        mainWindow.setSize(NextWindowSize[1] - 210, NextWindowSize[1]);
       }
     }
     currentWindowSize = mainWindow.getSize();
   }
 
-  function setWindow(){
-    return{
+  function setWindow() {
+    return {
       x: bounds_info.x,
       y: bounds_info.y,
       width: bounds_info.width,
       height: bounds_info.height,
-      minWidth:740,
-      minHeight:907,
-      icon: 'images/icon.png',
+      minWidth: 740,
+      minHeight: 907,
+      icon: "images/icon.png",
       title: "Pomodoro",
       useContentSize: true,
       webPreferences: {
         nodeIntegration: false,
         contextIsolation: true,
-        preload: join(__dirname, 'preload.js'),
+        preload: join(__dirname, "preload.js"),
       },
-    }
+    };
   }
 
   testDbFunction();
 
   const url = isDev
-    ? 'http://localhost:8000/'
+    ? "http://localhost:8000/"
     : format({
-      pathname: join(__dirname, '../renderer/out/index.html'),
-      protocol: 'file:',
-      slashes: true,
-    });
+        pathname: join(__dirname, "../renderer/out/index.html"),
+        protocol: "file:",
+        slashes: true,
+      });
   //delete menu bar
   mainWindow.setMenu(null);
 
-  mainWindow.loadURL(url)
-})
+  mainWindow.loadURL(url);
+});
 
 // Quit the app once all windows are closed
-app.on('window-all-closed', app.quit)
+app.on("window-all-closed", app.quit);
 
 // listen the channel `message` and resend the received message to the renderer process
-ipcMain.handle("dialogMsg", (_event,_data) => {
-  const NOTIFICATION_TITLE = 'Pomodoro'
-  const NOTIFICATION_BODY = _data
-  new Notification({ title: NOTIFICATION_TITLE, body: NOTIFICATION_BODY, silent:  true }).show()
-})
-
+ipcMain.handle("dialogMsg", (_event, _data) => {
+  const NOTIFICATION_TITLE = "Pomodoro";
+  const NOTIFICATION_BODY = _data;
+  new Notification({
+    title: NOTIFICATION_TITLE,
+    body: NOTIFICATION_BODY,
+    silent: true,
+  }).show();
+});
 
 ipcMain.handle("testdb", (_event) => {
   testDbFunction();
-})
+});
 
 ipcMain.handle("dbupdate", (_event) => {
   dbupdate();
-})
+});
 
 ipcMain.handle("getAllData", (_event) => {
   return getAllData();
-})
+});
 
+const db = require("electron-db");
+const path = require("path");
+const savePath: any = path.join("./", "");
 
-
-const db = require('electron-db');
-const path = require('path');
-const savePath:any = path.join("./","");
-
-interface POMODORODATA{
-  date: string,
-  count:number,
+interface POMODORODATA {
+  date: string;
+  count: number;
 }
 
 export const testDbFunction = () => {
-  if(!db.tableExists('PomodoroHistory',savePath)) {
-    db.createTable('PomodoroHistory',savePath,(success:boolean,msg:string) => {
-      if(success) {
-        console.log(msg);
-      } else {
-        console.log("failed to createTable. "+msg);
+  if (!db.tableExists("PomodoroHistory", savePath)) {
+    db.createTable(
+      "PomodoroHistory",
+      savePath,
+      (success: boolean, msg: string) => {
+        if (success) {
+          console.log(msg);
+        } else {
+          console.log("failed to createTable. " + msg);
+        }
       }
-    });
+    );
   } else {
     console.log("already create PomodoroHistory table");
   }
 };
 
-export const getAllData = () : POMODORODATA[] | boolean => {
-  let result : POMODORODATA[] | boolean = false;
-  if(db.tableExists('PomodoroHistory',savePath)) {
-    db.getAll( 'PomodoroHistory', savePath ,(success:boolean,contents:POMODORODATA[]) => {
-      if(success) {
-        console.log("getAll success");
-        console.log(contents);
-        result = contents;
-      } else {
-        console.log("getAll failed");
-        result = false;
+export const getAllData = (): POMODORODATA[] | boolean => {
+  let result: POMODORODATA[] | boolean = false;
+  if (db.tableExists("PomodoroHistory", savePath)) {
+    db.getAll(
+      "PomodoroHistory",
+      savePath,
+      (success: boolean, contents: POMODORODATA[]) => {
+        if (success) {
+          console.log("getAll success");
+          console.log(contents);
+          result = contents;
+        } else {
+          console.log("getAll failed");
+          result = false;
+        }
       }
-    });
+    );
   } else {
     console.log("table is not exist");
     result = false;
   }
   return result;
-}
+};
 
 export const dbupdate = () => {
   const currentDate = new Date();
-  const today: string = ( String(currentDate.getFullYear()) + "-" + ("0" + String(currentDate.getMonth() + 1 )).slice(-2) + "-" + ("0" + String(currentDate.getDate())).slice(-2));
-  if(db.tableExists('PomodoroHistory',savePath)) {
-    db.getRows('PomodoroHistory',savePath ,{date: today},(success:boolean, contents:POMODORODATA[] ) => {
-      if(success) {
-        console.log("getRows success");
-        console.log(contents);
-        if(contents.length > 0) {
-          let newCount: number = contents[0].count;
-          newCount++;
-          db.updateRow('PomodoroHistory', savePath, {"date":today}, {"count": newCount },  (successupdaterow:boolean,message:string) => {
-            if(successupdaterow) {
-              console.log("updatePomodoroHistoryData.updateRow success."+message);
-            } else {
-              console.log("updatePomodoroHistoryData.updateRow failed."+message);
-            }
-          });
-        } else {
-        let todaysFirstPomodoro : POMODORODATA= {count:1,date:today};
-        db.insertTableContent('PomodoroHistory',savePath,todaysFirstPomodoro,(success:boolean,message:string) => {
-          if(success) {
-            console.log("insertTableContent success : "+message);
+  const today: string =
+    String(currentDate.getFullYear()) +
+    "-" +
+    ("0" + String(currentDate.getMonth() + 1)).slice(-2) +
+    "-" +
+    ("0" + String(currentDate.getDate())).slice(-2);
+  if (db.tableExists("PomodoroHistory", savePath)) {
+    db.getRows(
+      "PomodoroHistory",
+      savePath,
+      { date: today },
+      (success: boolean, contents: POMODORODATA[]) => {
+        if (success) {
+          console.log("getRows success");
+          console.log(contents);
+          if (contents.length > 0) {
+            let newCount: number = contents[0].count;
+            newCount++;
+            db.updateRow(
+              "PomodoroHistory",
+              savePath,
+              { date: today },
+              { count: newCount },
+              (successupdaterow: boolean, message: string) => {
+                if (successupdaterow) {
+                  console.log(
+                    "updatePomodoroHistoryData.updateRow success." + message
+                  );
+                } else {
+                  console.log(
+                    "updatePomodoroHistoryData.updateRow failed." + message
+                  );
+                }
+              }
+            );
           } else {
-            console.log("insertTableContent failed : "+message);
+            let todaysFirstPomodoro: POMODORODATA = { count: 1, date: today };
+            db.insertTableContent(
+              "PomodoroHistory",
+              savePath,
+              todaysFirstPomodoro,
+              (success: boolean, message: string) => {
+                if (success) {
+                  console.log("insertTableContent success : " + message);
+                } else {
+                  console.log("insertTableContent failed : " + message);
+                }
+              }
+            );
           }
-        });
+        } else {
+          console.log("getRows failed");
         }
-      } else {
-        console.log("getRows failed");
       }
-    });
+    );
   }
-}
+};
--- renderer/pages/index.tsx
+++ renderer/pages/index.tsx
@@ -1,4 +1,4 @@
-import React,{ useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { work_icon, rest_icon, stop_icon } from '../components/icon'
 import css from '../styles/index.module.css'
 import Heatmap from '../components/heatmap'
@@ -10,7 +10,7 @@ import VolumeDown from '@mui/icons-material/VolumeDown';
 import VolumeUp from '@mui/icons-material/VolumeUp';
 
 
-type Datas = {count: number, date: string, id:number}[]
+type Datas = { count: number, date: string, id: number }[]
 
 const Element: React.FC<Datas> = () => {
     const BeginDate = new Date();
@@ -18,28 +18,29 @@ const Element: React.FC<Datas> = () => {
     BeginDate.setMonth(BeginDate.getMonth() - 11);
     BeginDate.setDate(1);
     let [TodaysPomodoroCount, setTodaysPomodoroCount] = useState("00");
-     useEffect(() => {
-        window.electron.getAllData().then(result =>{
+    useEffect(() => {
+        const currentDate = new Date();
+        const dateCheck: string = (String(currentDate.getFullYear()) + "-" + ("0" + String(currentDate.getMonth() + 1)).slice(-2) + "-" + ("0" + String(currentDate.getDate())).slice(-2));
+        window.electron.getAllData().then(result => {
             setCliantJson(result);
-            if( result[result.length - 1] !== undefined){
-                setTodaysPomodoroCount(result[result.length - 1].count === undefined ? "0" : ("0" +  String(result[result.length - 1].count)).slice(-2) );
+            if (result[result.length - 1] !== undefined && dateCheck === result[result.length - 1].date) {
+                setTodaysPomodoroCount(("0" + String(result[result.length - 1].count)).slice(-2));
             }
-        }) 
+        })
     }, []);
 
 
-    function jsonNullCheck(){
-       if(cliantJson == null){
-           return;
-       }else
-       {
-            return(
+    function jsonNullCheck() {
+        if (cliantJson == null) {
+            return;
+        } else {
+            return (
                 <Heatmap
-                beginDate={(BeginDate)} // optional
-                data={cliantJson}
+                    beginDate={(BeginDate)} // optional
+                    data={cliantJson}
                 />
-           )
-       }
+            )
+        }
     }
 
 
@@ -49,7 +50,7 @@ const Element: React.FC<Datas> = () => {
     let second: string = `0${count % 60}`.slice(-2);
     const workTime: number = 1500;
     const breakTime: number = 300;
-    
+
     //status = 0 initialStatus; status = 1 workTime; status = 2 breakTime
     let [status, setStatus] = useState(0);
     let [stopCount, setStopCount] = useState(null);
@@ -76,27 +77,27 @@ const Element: React.FC<Datas> = () => {
         setVolume(newValue);
     };
 
-    function startmp3(){
+    function startmp3() {
         let startAudio = new Audio()
-        startAudio.src = "./music/bell.mp3" 
-        startAudio.volume = volume/100;
+        startAudio.src = "./music/bell.mp3"
+        startAudio.volume = volume / 100;
         startAudio.play()
     }
 
-    function finishmp3(){
+    function finishmp3() {
         let finishAudio = new Audio()
-        finishAudio.src = "./music/gong.mp3" 
-        finishAudio.volume = volume/100
+        finishAudio.src = "./music/gong.mp3"
+        finishAudio.volume = volume / 100
         finishAudio.play()
     }
 
-    function stopmp3(){
+    function stopmp3() {
         let stopAudio = new Audio()
-        stopAudio.src = "./music/jingle.mp3" 
-        stopAudio.volume = volume/100
+        stopAudio.src = "./music/jingle.mp3"
+        stopAudio.volume = volume / 100
         stopAudio.play()
     }
- 
+
 
 
     //DataBase
@@ -105,44 +106,44 @@ const Element: React.FC<Datas> = () => {
         await window.electron.dbupdate();
     };
 
-    function addPomodoroCount(){
+    function addPomodoroCount() {
         dbupdate();
         const currentDate = new Date();
-        const dateChange: string = ( String(currentDate.getFullYear()) + "-" + ("0" + String(currentDate.getMonth() + 1 )).slice(-2) + "-" + ("0" + String(currentDate.getDate())).slice(-2));
-        if(cliantJson[cliantJson.length - 1] == null){
-            cliantJson.push( {date: dateChange, count: 1 , id: 1234567})
-        }else{
-            if (dateChange == cliantJson[cliantJson.length -1].date){
-                cliantJson[cliantJson.length -1].count++;
-            }else{
-                cliantJson.push( {date: dateChange, count: 1 , id: 1234567})
+        const dateChange: string = (String(currentDate.getFullYear()) + "-" + ("0" + String(currentDate.getMonth() + 1)).slice(-2) + "-" + ("0" + String(currentDate.getDate())).slice(-2));
+        if (cliantJson[cliantJson.length - 1] == null) {
+            cliantJson.push({ date: dateChange, count: 1, id: 1234567 })
+        } else {
+            if (dateChange == cliantJson[cliantJson.length - 1].date) {
+                cliantJson[cliantJson.length - 1].count++;
+            } else {
+                cliantJson.push({ date: dateChange, count: 1, id: 1234567 })
             }
         }
-        setTodaysPomodoroCount(("0" + String(cliantJson[cliantJson.length -1].count)).slice(-2));
+        setTodaysPomodoroCount(("0" + String(cliantJson[cliantJson.length - 1].count)).slice(-2));
     }
- 
- 
+
+
 
 
     //Timer
-    function setTime(time){
+    function setTime(time) {
         setCount(time);
     }
 
-    function countdown(){
+    function countdown() {
         setCount(count - 1);
     }
 
 
 
     useEffect(() => {
-        if(status !== 0){
-            if (count >= 0 && (status === 1 || status === 2 )) {
+        if (status !== 0) {
+            if (count >= 0 && (status === 1 || status === 2)) {
                 const timerId = setInterval(countdown, 1000);
                 return () => clearInterval(timerId);
             }
             if (count <= 0) {
-                if(stopCount == true){
+                if (stopCount == true) {
                     setTime(workTime)
                     setStatus(0);
                     setStopCount(null);
@@ -151,7 +152,7 @@ const Element: React.FC<Datas> = () => {
                     if (status === 1) {
                         addPomodoroCount();
                     }
-                }else{
+                } else {
                     if (status === 1) {
                         setTime(breakTime)
                         setStatus(2);
@@ -165,16 +166,16 @@ const Element: React.FC<Datas> = () => {
                         startmp3();
                     }
                 }
-           }
+            }
         }
-    },);
+    });
 
 
-    function StatusManagement(){
-        switch(status){
+    function StatusManagement() {
+        switch (status) {
             case 0:
-                setTime(workTime);  
-                setStatus(1); 
+                setTime(workTime);
+                setStatus(1);
                 break;
             case 1:
             case 2:
@@ -186,11 +187,11 @@ const Element: React.FC<Datas> = () => {
     const work_color: string = "#ff4d2d";
     const rest_color: string = "#11ad11";
     const stop_color: string = "#214098";
-    let gauge = Math.floor( count / (status === 1 ? workTime/25 : breakTime/25));
+    let gauge = Math.floor(count / (status === 1 ? workTime / 25 : breakTime / 25));
 
     const dots = () => {
         const range = (start: number, end: number) => [...Array((end - start) + 1)].map((_, i) => start + i);
-       return (
+        return (
             <>
                 {
                     range(0, 24).map((num) => {
@@ -250,24 +251,24 @@ const Element: React.FC<Datas> = () => {
     }
     return (
         <>
-          <div className={css.content}>
+            <div className={css.content}>
 
-           <div className={css.calndar} onClick={() => { StatusManagement(); }}>
-            <div className="square">
-                {dots()}
-                {information_area()}
-              </div>
-           </div>
-            <Box sx={{ width: 200 }}>
-            <Stack spacing={2} direction="row" sx={{ mb: 1 }} alignItems="center">
-                <VolumeDown />
-                <Slider aria-label="Volume" value={volume} onChange={handleChange} />
-                <VolumeUp />
-            </Stack>
-            </Box>
-            {jsonNullCheck()}
-         </div>
-       </>
+                <div className={css.calndar} onClick={() => { StatusManagement(); }}>
+                    <div className="square">
+                        {dots()}
+                        {information_area()}
+                    </div>
+                </div>
+                <Box sx={{ width: 200 }}>
+                    <Stack spacing={2} direction="row" sx={{ mb: 1 }} alignItems="center">
+                        <VolumeDown />
+                        <Slider aria-label="Volume" value={volume} onChange={handleChange} />
+                        <VolumeUp />
+                    </Stack>
+                </Box>
+                {jsonNullCheck()}
+            </div>
+        </>
     )
 }
 
